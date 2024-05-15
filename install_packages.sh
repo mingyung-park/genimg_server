c9e3f9bb073a8c00ab2ae9efd39903b779363712
mingyung-park <kateking001130@threepark-genai-22.c.oceanic-works-419607.internal>
2024-05-15 07:15:31 +0000

    final code

---
+++ install_packages.sh
@@ -0,0 +1,50 @@
+# Flask 설치
+pip install flask
+
+# diffusers 설치
+pip install -i https://test.pypi.org/simple/ diffusers
+
+# accelerate 설치
+pip install 'accelerate>=0.16.0'
+
+# torchvision 설치
+pip install torchvision
+
+# transformers 설치
+pip install 'transformers>=4.25.1'
+
+# datasets 설치
+pip install datasets
+
+# ftfy 설치
+pip install ftfy
+
+# tensorboard 설치
+pip install tensorboard
+
+# Jinja2 설치
+pip install Jinja2
+
+# peft 설치
+pip install 'peft==0.7.0'
+
+# boto3 설치
+pip install boto3
+
+# 보안 라이브러리
+pip install werkzeug
+
+# dotenv로 환경변수 관리
+pip install python-dotenv
+
+# async
+pip install Flask[async]
+
+# kobert
+pip install gluonnlp pandas tqdm
+pip install 'numpy==1.23.1'
+pip install 'mxnet==1.9.1'
+pip install sentencepiece
+pip install torch
+pip install 'git+https://github.com/SKTBrain/KoBERT.git#egg=kobert_tokenizer&subdirectory=kobert_hf'
+pip install -U scikit-learn
